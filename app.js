9fe13b5a598c773062bf4b913e86375bb160eae6
younghookim <younghoo88@gmail.com>
2015-12-27 18:08:37 +0900

    default mount point였던 users.js 관련 부분 삭제, index.js는 일단 놔둠. database 연동을 위한 mysql module 설치

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 // Custom mount point
 var user = require('./routes/user');
